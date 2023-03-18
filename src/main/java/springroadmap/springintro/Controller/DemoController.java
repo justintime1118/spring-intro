8ee258cb08b6606170fdefc92ff0117db5827d7c
justinYoo <ugh1118@gmail.com>
2023-03-18 18:28:09 +0900

    view 환경설정

---
+++ src/main/java/springroadmap/springintro/Controller/DemoController.java
@@ -0,0 +1,15 @@
+package springroadmap.springintro.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("wassup")
+    public String hello(Model model) {
+        model.addAttribute("data", "wassup~~");
+        return "wassup";
+    }
+}

01463e40f4f9e62d3a54e6065eb224aae4d1f76a
justinYoo <ugh1118@gmail.com>
2023-03-19 19:20:41 +0900

    Service 구현 및 테스트코드 작성

---
+++ src/main/java/springroadmap/springintro/service/MemberService.java
@@ -0,0 +1,42 @@
+package springroadmap.springintro.service;
+
+import springroadmap.springintro.domain.Member;
+import springroadmap.springintro.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
+    public Long join(Member member) {
+        validateDuplicateName(member); // 중복회원 검증
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateName(Member member) {
+        // Optional 객체를 사용하면 이렇게 단순하게 코드를 작성할 수 있다
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체회원조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    //ID로 회원조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/test/java/springroadmap/springintro/service/MemberServiceTest.java
@@ -0,0 +1,68 @@
+package springroadmap.springintro.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import springroadmap.springintro.domain.Member;
+import springroadmap.springintro.repository.MemoryMemberRepository;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemoryMemberRepository memberRepository;
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clear();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("Lionel");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        Member memberFound = memberRepository.findById(savedId).get();
+        assertEquals(member.getName(), memberFound.getName());
+    }
+
+    @Test
+    void dupJoin() {
+        //given
+        Member member1 = new Member();
+        member1.setName("Lionel");
+
+        Member member2 = new Member();
+        member2.setName("Lionel");
+
+        memberService.join(member1);
+
+        //when
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2)); // 예외가 발생해야 함
+
+        //then
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
